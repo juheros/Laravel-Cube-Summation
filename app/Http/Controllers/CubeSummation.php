8ef5664f499232d013ae48ab0137c2525e70e15a
Julian Hernandez Osorio <julian.hernandez@comred.com.co>
2016-09-28 17:27:48 -0500

    Refactorizando las validaciones de entrada de datos en los controladores.

--- app/Http/Controllers/CubeSummation.php
+++ app/Http/Controllers/CubeSummation.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Exception;
 use InvalidArgumentException;
 use Response;
+use Validator;
 use Illuminate\Http\Request;
 use App\Cube;
 
@@ -17,15 +18,18 @@ class CubeSummation extends Controller
   */
   public function start(Request $request)
   {
-    $test_cases_num = (int) $request->input('test_cases_num');
+    $validator = Validator::make($request->all(), [
+      'test_cases_num' => 'required|integer|between:1,50'
+    ]);
 
     $status = 500;
     $response = array('message' => 'Error desconocido.');
 
-    if ($test_cases_num < 1 || $test_cases_num > 50) {
-      $status = 400;
-      $response = array('message' => 'Valor no válido para T.');
-    } else {
+    if($validator->fails()){
+        $status = 400;
+        $response = array('message' => 'Argumentos no válidos para esta solicitud.');
+    }else {
+        $test_cases_num = (int) $request->input('test_cases_num');
       $request->session()->remove('cube');
       $request->session()->put('test_cases_num', $test_cases_num);
 
@@ -43,22 +47,26 @@ class CubeSummation extends Controller
   */
   public function startTestCase(Request $request)
   {
-    $cube_size = (int) $request->input('cube_size');
-    $querys_num = (int) $request->input('querys_num');
-    $test_cases_num = $request->session()->get('test_cases_num', 0);
-
+    $validator = Validator::make($request->all(), [
+        'cube_size' => 'required|integer|between:1,100',
+        'querys_num' => 'required|integer|between:1,1000',
+    ]);
     $status = 500;
     $response = array('message' => 'Error desconocido.');
+    $test_cases_num = $request->session()->get('test_cases_num', 0);
 
-    if ($test_cases_num <= 0) {
+    if($validator->fails()){
+        $status = 400;
+        $response = array('message' => 'Argumentos no válidos para esta solicitud.');
+
+    }else if ($test_cases_num <= 0) {
       $status = 401;
       $response = array('message' => 'Casos de prueba agotados.');
-    }
-    else if ($querys_num < 1 || $querys_num > 1000) {
-      $status = 400;
-      $response = array('message' => 'Valor no válido para M.');
+
     }else{
       try {
+        $cube_size = (int) $request->input('cube_size');
+        $querys_num = (int) $request->input('querys_num');
         $cube = new Cube($cube_size);
         $request->session()->put('querys_num', $querys_num);
         $request->session()->put('cube', $cube);
